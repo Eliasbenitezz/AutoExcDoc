4dc446e1fcd17bfc590f4ff70e3b3fd700dafa0a
Elias <benitezelias882@proton.me>
2025-05-04 21:28:36 -0300

    Creamos una carta de ejemplo para que puedan ver donde se van a escribir los datos. Tambien activamos el libro y la hoja donde se encuentran los datos requeridos, continuare con la escritura de dichos datos

--- app.py
+++ app.py
@@ -2,5 +2,24 @@ import openpyxl as op
 from datetime import datetime
 from docxtpl import DocxTemplate
 
-doc = DocxTemplate("Notas de alumnos.xlsx")
+input_file = "Notas de alumnos.xlsx"
+input_latter = "Carta.docx"
 
+try:
+    wordbook = op.load_workbook(input_file)
+    sheet_input = wordbook.active
+except FileNotFoundError:
+    print(f"ERROR: El archivo {input_file} no se encontro") 
+
+for indexrow, row in enumerate(sheet_input.iter_rows(min_row=2), start=2):
+    """Aqui estamos opteniendo los valores de las columnas, Teniendo en cuenta que las primeras filas
+    son los encabezados ósea, Nombre, Apellido. etc"""
+    nombre = row[0].value
+    apellido = row[1].value
+    nota = row[2].value
+    materia = row[3].value
+    
+    if nombre and apellido:
+        print(f'Procesando los datos de {nombre} {apellido}')
+        
+        
